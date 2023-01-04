36e7e6af96bb0b7c9379be4c8752e4caf0d824fa
MINJU KIM <yd5726@naver.com>
2023-01-04 18:40:45 +0900

    비밀번호 찾기
    비밀번호 찾기

--- LMS_KMJ/app/src/main/java/com/example/lms_kmj/LoginActivity.java
+++ LMS_KMJ/app/src/main/java/com/example/lms_kmj/LoginActivity.java
@@ -5,31 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
-    Button login_btn;
-    TextView join_tv;
+    TextView join_tv,login_tv,find_tv;
+    TextInputEditText id_et, pw_et;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login_btn = findViewById(R.id.login_btn);
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        id_et = findViewById(R.id.id_et);
+        pw_et = findViewById(R.id.id_pw);
+
+        // 로그인 버튼
+        login_tv = findViewById(R.id.login_tv);
+        login_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 아이디와 비밀번호 길이가 0이 아니면
+                /*
+                if(id_et.getText().toString().length() != 0 && pw_et.getText().toString().length() != 0){
+                    String id = id_et.getText().toString();
+                    String pw = pw_et.getText().toString();
+                }
+                */
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
 
+        // 회원가입 버튼
         join_tv = findViewById(R.id.join_tv);
         join_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 비밀번호 찾기 버튼
+        find_tv = findViewById(R.id.find_tv);
+        find_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, FindActivity.class);
                 startActivity(intent);
             }
         });
