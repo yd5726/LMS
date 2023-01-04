dc32db53b8005fc3fde9296137bc57b3a8d82537
MINJU KIM <yd5726@naver.com>
2023-01-05 01:42:57 +0900

    집에서 userid 조회
    집에서 userid 조회

--- middle/src/main/java/com/and/middle/LMSController.java
+++ middle/src/main/java/com/and/middle/LMSController.java
@@ -16,17 +16,28 @@ import lms_member.MemberVO;
 @RestController
 public class LMSController {
 	@Autowired @Qualifier("smart01") private SqlSession session;
+	
 	@RequestMapping("/and")
 	public String andController_test() {
 		return "and";
 	}
+	
 	@RequestMapping(value = "/home.mj", produces ="text/html;charset=UTF-8")
-	public String login() {
-		List<MemberVO> list = session.selectList("lms.login");
+	public String home() {
+		List<MemberVO> list = session.selectList("lms.home");
 		System.out.println("list.size() : " + list.size());
 		System.out.println("sql.selectList(\"lms.login\").size() : "
 				+ session.selectList("lms.login").size());
 		
 		return new Gson().toJson(list);
+	}
+	
+	// http://localhost:8080/smart/login.mj?userid=admin
+	@RequestMapping(value = "/login.mj", produces ="text/html;charset=UTF-8")
+	public String login(String userid) {
+		MemberVO member = session.selectOne("lms.login",userid);
+		System.out.println(member.getUserid()); // admin
+		
+		return new Gson().toJson(member); // {"userid":"admin"}
 	}
 }
