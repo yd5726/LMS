f219df6f57d502a28a8d2dcbbd43c317f9f84f4e
MINJU KIM <yd5726@naver.com>
2023-01-05 04:00:24 +0900

    AND : DrawerLayout - 물음표
    AND : DrawerLayout - 물음표

--- LMS_KMJ/app/src/main/java/com/example/lms_kmj/TTActivity.java
+++ LMS_KMJ/app/src/main/java/com/example/lms_kmj/TTActivity.java
@@ -1,14 +1,45 @@
 package com.example.lms_kmj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class TTActivity extends AppCompatActivity {
+    Toolbar top_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tt);
+
+        // 상단바
+        top_toolbar = findViewById(R.id.top_toolbar);
+        top_toolbar.setTitle("시간표");
+        top_toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.top_toolbar_more:
+                        Intent intent = new Intent(TTActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        // 상단바 뒤로가기 버튼
+        top_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
 }
