a515de4a5217ce4b3c2a346373c0b55414c4646e
MINJU KIM <yd5726@naver.com>
2023-01-10 01:04:11 +0900

    AND : 학원 일정 DB 조회
    AND : 학원 일정 DB 조회

--- LMS_KMJ/app/src/main/java/com/example/lms_kmj/drawer/AcCalendarActivity.java
+++ LMS_KMJ/app/src/main/java/com/example/lms_kmj/drawer/AcCalendarActivity.java
@@ -11,9 +11,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 
+import com.example.conn.CommonMethod;
 import com.example.lms_kmj.R;
+import com.example.lms_kmj.board.BoardVO;
 import com.example.lms_kmj.tt_recv.TTAdapter;
 import com.example.lms_kmj.tt_recv.TT_DTO;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
@@ -48,14 +52,32 @@ public class AcCalendarActivity extends AppCompatActivity {
                 Log.d("로그", "year: " + year);
                 Log.d("로그", "month: " + (month+1));
                 Log.d("로그", "dayOfMonth: " + dayOfMonth);
+                String writedate = year+"/"+(month+1)+"/"+dayOfMonth;
+                Log.d("로그", "writedate: "+writedate);
+                new CommonMethod().setParams("writedate",writedate)
+                        .sendPost("aclist.mj", new CommonMethod.CallBackResult() {
+                            @Override
+                            public void result(boolean isResult, String data) {
+                                ArrayList<BoardVO> aclist = new Gson().fromJson(data, new TypeToken<ArrayList<BoardVO>>(){}.getType());
+                                Log.d("로그", "aclist: "+aclist);
+                                for (int i = 0 ; i < aclist.size() ; i++){
+                                    String acWritedate = aclist.get(i).getWritedate();
+                                    String acContext = aclist.get(i).getContent();
+                                    list = new ArrayList<>();
+                                    list.add(new TT_DTO(acWritedate,"",acContext));
+                                    tt_recv_list.setAdapter(new TTAdapter(getLayoutInflater(), list));
+                                    tt_recv_list.setLayoutManager(new LinearLayoutManager(getApplicationContext(),RecyclerView.VERTICAL,false));
+                                }
+                            }
+                        });
             }
         });
 
         // 일정들
         tt_recv_list = findViewById(R.id.tt_recv_list);
-        list = new ArrayList<>();
-        list.add(new TT_DTO("1월 6일","3:00 ~ 5:02","일정 회의가 있습니다."));
-        tt_recv_list.setAdapter(new TTAdapter(getLayoutInflater(), list));
-        tt_recv_list.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+        //list = new ArrayList<>();
+        //list.add(new TT_DTO("1월 6일","3:00 ~ 5:02","일정 회의가 있습니다."));
+        //tt_recv_list.setAdapter(new TTAdapter(getLayoutInflater(), list));
+        //tt_recv_list.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
     }
 }
