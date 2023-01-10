230e9d76f677b31c91cae20e05eb4628874f5db8
MINJU KIM <yd5726@naver.com>
2023-01-10 20:19:09 +0900

    AND : 시간표 - ?
    AND : 시간표 - ?

--- LMS_KMJ/app/src/main/java/com/example/lms_kmj/TTActivity.java
+++ LMS_KMJ/app/src/main/java/com/example/lms_kmj/TTActivity.java
@@ -31,15 +31,29 @@ public class TTActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tt);
 
-        // 상단바
         tt_toolbar = findViewById(R.id.tt_toolbar);
+        linearLayouts.add(findViewById(R.id.ln_layout0));
         linearLayouts.add(findViewById(R.id.ln_layout1));
         linearLayouts.add(findViewById(R.id.ln_layout2));
         linearLayouts.add(findViewById(R.id.ln_layout3));
         linearLayouts.add(findViewById(R.id.ln_layout4));
         linearLayouts.add(findViewById(R.id.ln_layout5));
+        linearLayouts.add(findViewById(R.id.ln_layout6));
+        linearLayouts.add(findViewById(R.id.ln_layout7));
+        linearLayouts.add(findViewById(R.id.ln_layout8));
+        linearLayouts.add(findViewById(R.id.ln_layout9));
+        linearLayouts.add(findViewById(R.id.ln_layout10));
+        linearLayouts.add(findViewById(R.id.ln_layout11));
+        linearLayouts.add(findViewById(R.id.ln_layout12));
+        linearLayouts.add(findViewById(R.id.ln_layout13));
+        linearLayouts.add(findViewById(R.id.ln_layout14));
+        linearLayouts.add(findViewById(R.id.ln_layout15));
+        linearLayouts.add(findViewById(R.id.ln_layout16));
+        linearLayouts.add(findViewById(R.id.ln_layout17));
 
-        tt_toolbar.setTitle(LoginInfo.check_id+"의 시간표");
+        Common common = new Common();
+        // 상단바
+        tt_toolbar.setTitle(common.getLoginInfo().getMember_name()+"의 시간표");
 
         // 상단바 뒤로가기 버튼
         tt_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -49,8 +63,6 @@ public class TTActivity extends AppCompatActivity {
             }
         });
 
-        Common common = new Common();
-
         tt_toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -59,25 +71,64 @@ public class TTActivity extends AppCompatActivity {
                             .sendPost("ttlist.mj", new CommonMethod.CallBackResult() {
                                 @Override
                                 public void result(boolean isResult, String data) {
-                                    ArrayList<LectureVO> list = new Gson().fromJson(data , new TypeToken<ArrayList<LectureVO>>(){}.getType());
-                                    for (int i = 0 ; i < list.size() ; i++){
-                                        int index = 0 ;
-                                        if(list.get(i).getvDay().equals("월")){
-                                            index= 0;
-                                        }else if(list.get(i).getvDay().equals("화")){
-                                            index= 1;
-                                        }else if(list.get(i).getvDay().equals("수")){
-                                            index= 2;
-                                        }else if(list.get(i).getvDay().equals("목")){
-                                            index= 3;
-                                        }else if(list.get(i).getvDay().equals("금")){
-                                            index= 4;
-                                        }else{
-                                            index= list.size()+1;  // 여기 타면 오류
+                                    ArrayList<LectureVO> list = new Gson().fromJson(data, new TypeToken<ArrayList<LectureVO>>() {}.getType());
+                                    Log.d("로그", "list.size(): "+list.size());   // list.size():15
+                                    Log.d("로그", "list: "+list);
+                                    Log.d("로그", "getvDay(0): "+list.get(0).getvDay());
+                                    Log.d("로그", "getvDay(1): "+list.get(1).getvDay());
+                                    Log.d("로그", "getvDay(2): "+list.get(2).getvDay());
+                                    Log.d("로그", "getvDay(3): "+list.get(3).getvDay());
+                                    Log.d("로그", "getvDay(4): "+list.get(4).getvDay());
+                                    Log.d("로그", "getvDay(5): "+list.get(5).getvDay());
+                                    for (int i = 0; i < 3; i++) {
+                                    //for (int i = 0; i < Integer.parseInt(list.get(i).getTimetable_code()); i++) {
+                                        for (int k = 0; k < 6; k++) {
+                                            //linearLayouts.get(k).addView(getTextView("("+i+","+k+")"));
+                                            /*
+                                            int index = 0;
+                                            if (list.get(k).getvDay().equals("월") && Integer.parseInt(list.get(k).getTimetable_code()) == i) {
+                                                index = ((k+1)*1);
+                                                Log.d("로그", "i: "+i+",k: "+k+", index: "+index);
+                                            } else if (list.get(k).getvDay().equals("화")) {
+                                                index = ((k+1)*2);
+                                                Log.d("로그", "i: "+i+",k: "+k+", index: "+index);
+                                            } else if (list.get(k).getvDay().equals("수")) {
+                                                index = ((k+1)*3);
+                                                Log.d("로그", "i: "+i+",k: "+k+", index: "+index);
+                                            } else if (list.get(k).getvDay().equals("목")) {
+                                                index = ((k+1)*4);
+                                                Log.d("로그", "i: "+i+",k: "+k+", index: "+index);
+                                            } else if (list.get(k).getvDay().equals("금")) {
+                                                index = ((k+1)*5);
+                                                Log.d("로그", "i: "+i+",k: "+k+", index: "+index);
+                                            } else {
+                                                index = 12;  // 여기 타면 오류
+                                            }
+                                            linearLayouts.get(index).addView(
+                                                    getTextView(list.get(index).getLecture_name())
+                                            );
+                                            */
+
+                                            /*
+                                            int index = 0;
+                                            if (list.get(i).getvDay().equals("월") && Integer.parseInt(list.get(i).getTimetable_code())==1) {
+                                                index = 0;
+                                            } else if (list.get(i).getvDay().equals("화")) {
+                                                index = 1;
+                                            } else if (list.get(i).getvDay().equals("수")) {
+                                                index = 2;
+                                            } else if (list.get(i).getvDay().equals("목")) {
+                                                index = 3;
+                                            } else if (list.get(i).getvDay().equals("금")) {
+                                                index = 4;
+                                            } else {
+                                                index = list.size() + 1;  // 여기 타면 오류  //list.size():15
+                                            }
+                                            linearLayouts.get(index).addView(
+                                                    getTextView(list.get(i).getLecture_name())
+                                            );
+                                            */
                                         }
-                                        linearLayouts.get(index).addView(
-                                                getTextView(list.get(i).getTimetable_code()+"교시\n"+list.get(i).getLecture_name())
-                                        );
                                     }
                                 }
                             });
@@ -86,7 +137,7 @@ public class TTActivity extends AppCompatActivity {
                 return false;
             }
         });
-    }//onCreate()       // id: teacher1  , pw: 000aA
+    }//onCreate()
 
     TextView getTextView(String data){
         TextView temp_tv = new TextView(this);
